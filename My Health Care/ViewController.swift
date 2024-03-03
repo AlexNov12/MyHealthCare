b20470e0f8c8fcdea694074ff248979646df72da
AlexNov12 <nov.alex96@gmail.com>
2024-03-03 20:21:37 +0300

    Initial Commit

---
+++ My Health Care/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Health Care
+//
+//  Created by Александр Новиков on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
