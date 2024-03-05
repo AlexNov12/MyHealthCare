e4c8429179bae67d2e42ab81551ad2e2e4e7e9d4
AlexNov12 <nov.alex96@gmail.com>
2024-03-05 22:19:15 +0300

    Added storyboards

---
+++ My Health Care/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  My Health Care
+//
+//  Created by Александр Новиков on 05.03.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ My Health Care/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  My Health Care
+//
+//  Created by Александр Новиков on 05.03.2024.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
