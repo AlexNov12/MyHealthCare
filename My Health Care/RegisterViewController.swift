d694e3a10d2208f4ab83597676a61f3dfd05969f
AlexNov12 <nov.alex96@gmail.com>
2024-03-06 22:21:05 +0300

    Added segues

--- My Health Care/RegisterViewController.swift
+++ My Health Care/RegisterViewController.swift
@@ -2,7 +2,7 @@
 //  RegisterViewController.swift
 //  My Health Care
 //
-//  Created by Александр Новиков on 05.03.2024.
+//  Created by Александр Новиков on 06.03.2024.
 //
 
 import UIKit
@@ -14,6 +14,16 @@ class RegisterViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
--- My Health Care/WelcomeViewController.swift
+++ My Health Care/WelcomeViewController.swift
@@ -2,7 +2,7 @@
 //  WelcomeViewController.swift
 //  My Health Care
 //
-//  Created by Александр Новиков on 03.03.2024.
+//  Created by Александр Новиков on 06.03.2024.
 //
 
 import UIKit
@@ -11,9 +11,8 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
